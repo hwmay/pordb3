3281d17b2271057b933811c81aefba70bc133daf
Hans Werner May <nc-mayha2@netcologne.de>
2014-06-19 19:25:42 +0200

    New function: adding scenes without scenes in IAFD is now possible

--- pypordb_save_movie_data.py
+++ pypordb_save_movie_data.py
@@ -36,23 +36,72 @@ class SaveMovieData(QtGui.QDialog):
 		scenes = []
 		anfang = self.text.find("Scene Breakdowns")
 		if anfang < 0:
-			self.app.restoreOverrideCursor()
-			message = QtGui.QMessageBox.critical(self, self.trUtf8("Error "), self.trUtf8("Seams there are no scenes on the IAFD"))
-			return
-		anfang_scene = self.text.find("Scene ", anfang + 1)
-		if anfang_scene < 0:
-			self.app.restoreOverrideCursor()
-			message = QtGui.QMessageBox.critical(self, self.trUtf8("Error "), self.trUtf8("Seams there are no scenes on the IAFD"))
-			return
-		while True:
+			scenes = self.get_actors()
+			print (scenes)
+		else:
 			anfang_scene = self.text.find("Scene ", anfang + 1)
-			ende_scene = self.text.find(". ", anfang_scene + 1)
 			if anfang_scene < 0:
-				break
-			scene = self.text[anfang_scene : ende_scene].strip()
-			anfang = self.text.find(". ", anfang_scene + 1)
-			ende = self.text.find("</li>", anfang)
-			darsteller = self.text[anfang + 2:ende].strip()
-			scenes.append((scene, darsteller))
+				self.app.restoreOverrideCursor()
+				message = QtGui.QMessageBox.critical(self, self.trUtf8("Error "), self.trUtf8("Seams there are no scenes on the IAFD"))
+				return
+			while True:
+				anfang_scene = self.text.find("Scene ", anfang + 1)
+				ende_scene = self.text.find(". ", anfang_scene + 1)
+				if anfang_scene < 0:
+					break
+				scene = self.text[anfang_scene : ende_scene].strip()
+				anfang = self.text.find(". ", anfang_scene + 1)
+				ende = self.text.find("</li>", anfang)
+				darsteller = self.text[anfang + 2:ende].strip()
+				scenes.append((scene, darsteller))
 			
 		return movie, alternatives, scenes
+	
+	def get_actors(self):
+		anfang_actresses = self.text.find("<h3>Actresses")
+		anfang_actors = self.text.find("<h3>Actors")
+		ende_komplett = self.text.find("correct.asp")
+		if anfang_actors < 0:
+			anfang_actors = len(self.text)
+		if anfang_actresses < 0 and anfang_actors < 0:
+			self.app.restoreOverrideCursor()
+			message = QtGui.QMessageBox.critical(self, self.trUtf8("Error "), self.trUtf8("Seams there are no scenes/actors on the IAFD"))
+			return
+		scenes = []
+		# get the actresses
+		anfang = anfang_actresses
+		scene = "Scene 0"
+		darsteller = ""
+		while True:
+			anfang_actor = self.text.find('.htm">', anfang + 1)
+			ende_actor = self.text.find("</a>", anfang_actor + 1)
+			if anfang_actor < 0 or anfang_actor > ende_komplett or anfang_actors > 0 and anfang_actor > anfang_actors:
+				break
+			if darsteller:
+				darsteller += ", "
+			darsteller += self.text[anfang_actor + 6: ende_actor].strip()
+			alias = darsteller.find("(as ")
+			if alias > 0:
+				darsteller = darsteller[0 : alias].strip()
+			anfang = ende_actor
+		scenes.append((scene, darsteller))
+		# get the actors
+		anfang = anfang_actors
+		scene = "Scene 0"
+		darsteller = ""
+		while True:
+			anfang_actor = self.text.find('.htm">', anfang + 1)
+			ende_actor = self.text.find("</a>", anfang_actor + 1)
+			if anfang_actor < 0 or anfang_actor > ende_komplett:
+				break
+			if darsteller:
+				darsteller += ", "
+			darsteller += self.text[anfang_actor + 6: ende_actor].strip()
+			alias = darsteller.find("(as ")
+			if alias > 0:
+				darsteller = darsteller[0 : alias].strip()
+			anfang = ende_actor
+		scenes.append((scene, darsteller))
+			
+		return scenes
+	

30b8974d40666218fe16acdd40b9eea19680e25b
Hans Werner May <nc-mayha2@netcologne.de>
2014-12-22 21:57:55 +0100

    Version 1.8.2

--- pordb.py
+++ pordb.py
@@ -47,7 +47,7 @@ size_darsteller = QtCore.QSize(1920, 1080)
 dbname = "por"
 initial_run = True
 
-__version__ = "1.8.1"
+__version__ = "1.8.2"
 file_version = "https://github.com/hwmay/pordb3/blob/master/version"
 
 # Make a connection to the database and check to see if it succeeded.

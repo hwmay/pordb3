3c078b53d5bbdcda92db3ff664154dba2740d89f
Hans Werner May <nc-mayha2@netcologne.de>
2015-08-04 01:38:21 +0200

    New function: added some new icons

--- py_pordb_backup.py
+++ py_pordb_backup.py
@@ -6,7 +6,7 @@ import os
 import subprocess
 
 DBNAME = "por"
-verzeichnis = os.path.expanduser("~") +os.sep +"Dokumente"
+verzeichnis = os.path.expanduser("~") +os.sep + "Dokumente"
 
 subprocess.check_output(["/usr/bin/vacuumdb", "--analyze", "por"], universal_newlines=True)
 subprocess.check_output(["pg_dump", DBNAME, "-f", os.path.join(verzeichnis, DBNAME + ".sql")], universal_newlines=True)

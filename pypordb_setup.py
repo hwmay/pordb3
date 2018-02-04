933b1ff37f1ce0e70083b8556549d4984b114fb2
Hans Werner May <nc-mayha2@netcologne.de>
2018-02-04 21:47:35 +0100

    Setup upgraded to Qt5

--- pypordb_setup.py
+++ pypordb_setup.py
@@ -1,6 +1,25 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+'''
+    Copyright 2012-2018 HWM
+    
+    This file is part of PorDB3.
+
+    PorDB3 is free software: you can redistribute it and or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    PorDB3 is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Foobar.  If not, see <http:  www.gnu.org licenses >.
+'''
+
 import sys
 import os
 from PyQt5 import QtGui, QtCore, QtWidgets
@@ -12,10 +31,10 @@ class Dialog(QtWidgets.QDialog, Dialog):
         QtWidgets.QDialog.__init__(self)
         self.setupUi(self)   
         
-        self.connect(self.pushButtonNext, QtCore.SIGNAL("clicked()"), self.onNext)
-        self.connect(self.pushButtonBack, QtCore.SIGNAL("clicked()"), self.onBack)
-        self.connect(self.pushButtonDir, QtCore.SIGNAL("clicked()"), self.onDirectory)
-        self.connect(self.pushButtonZip, QtCore.SIGNAL("clicked()"), self.onZipFile)
+        self.pushButtonNext.clicked.connect(self.onNext)
+        self.pushButtonBack.clicked.connect(self.onBack)
+        self.pushButtonDir.clicked.connect(self.onDirectory)
+        self.pushButtonZip.clicked.connect(self.onZipFile)
         
         self.neuer_tab = 0
         self.verzeichnis = os.path.expanduser("~")

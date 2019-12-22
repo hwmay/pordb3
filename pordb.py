1fe49465259b6d9fd6ffa32ff2a581c43649326e
Hans Werner May <nc-mayha2@netcologne.de>
2019-12-22 22:47:21 +0100

    Version 2.0.0: Upgrade to QT5

--- pordb.py
+++ pordb.py
@@ -66,7 +66,7 @@ size_darsteller = QtCore.QSize(1920, 1080)
 
 DBNAME = "por"
 
-__version__ = "1.10.1"
+__version__ = "2.0.0"
 FILE_VERSION = "https://github.com/hwmay/pordb3/blob/master/version"
 IMAGE_FILES = (".jpg", ".jpeg", ".png")
 
